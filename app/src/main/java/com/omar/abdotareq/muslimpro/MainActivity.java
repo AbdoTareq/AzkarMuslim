dd4efb44ada3caa44c795dfb287fb7003c2e9b3f
Abdo Tareq <abdotarekfathy@gmail.com>
2019-08-11 18:29:30 +0200

    first_commit

---
+++ app/src/main/java/com/omar/abdotareq/muslimpro/MainActivity.java
@@ -0,0 +1,21 @@
+package com.omar.abdotareq.muslimpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+/**
+ * An intro that has azkar and ahadeth
+ * */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
